9fb1d44a0873cb92a5ce32ddc7392d1d6ced5036
DisonGo <jokerj224@gmail.com>
2021-10-20 00:06:30 +0300

    fixex for heroku

--- server.js
+++ server.js
@@ -1,28 +1,36 @@
 const express = require('express')
-const {MongoClient} = require('mongodb')
+const {
+    MongoClient
+} = require('mongodb')
 const cors = require('cors')
 const projectRouter = require('./scripts/projectRouter')
 const userRouter = require('./scripts/userRouter')
 
-
+function requireHTTPS(req, res, next) {
+    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+        return res.redirect('https://' + req.get('host') + req.url);
+    }
+    next();
+}
 
 const mongoUrl = "mongodb+srv://mpuCADdb:b7TmLx7UR7wq3KB@learningdbcluster.0oiga.mongodb.net/CAD?";
 
-const client = new MongoClient(mongoUrl) 
+const client = new MongoClient(mongoUrl)
 const app = express()
 
-async function main(){
+async function main() {
     await client.connect()
-    
-    var port = process.env['app_port'] || 8080
+
+    const port = process.env.PORT || 8080;
     app.use(cors())
-    .use(express.json())
-    .use(express.urlencoded({
-        extended:true
-    }))
-    .use(projectRouter(client))
-    .use(userRouter(client))
-    app.listen(port,()=>{
+        .use(express.json())
+        .use(express.urlencoded({
+            extended: true
+        }))
+        .use(projectRouter(client))
+        .use(userRouter(client))
+    if(app.get('env')!="development")app.use(requireHTTPS);
+    app.listen(port, () => {
         console.log(`Listening to ${port}`);
     })
 }
