9013e43274c5ccd7179a4098fe7f0d8de4f77452
DisonGo <jokerj224@gmail.com>
2021-10-21 13:00:37 +0300

    login api upload

--- scripts/login.js
+++ scripts/login.js
@@ -1,92 +1,60 @@
 const express = require('express')
 const chalk = require('chalk')
+const jwt= require('jsonwebtoken');
 const time = require('./consTime')
+const fs = require("fs");
+const RSA_PRIVATE_KEY = fs.readFileSync('./login/private.key');
+let db, col
 
-
-
-function teamWorkerRouter(client) {
-  let db = client.db("CAD")
-  let col = db.collection("teamWorker")
-
-  const router = new express.Router()
-  router.post('/teamWorker', (req, res, next) => {
-    try {
-      col.insertOne({
-        name: req.body.name,
-        surname: req.body.surname,
-        project: req.body.project,
-        imgLink: req.body.imgLink
-      }).then(() => {
-        console.log(`${time.getTime()}` + chalk.green `POST` + ":200");
-        res.status(200).json({
-          status: 'ok'
-        });
-      })
-    } catch (error) {
-      console.log(`${time.getTime()}` + chalk.red `POST` + ":500");
-      console.error(error);
-      res.status(500).json({
-        status: 'error'
-      });
-    }
-  });
-  router.get('/teamWorker', function (req, res, next) {
-    try {
-      col.find({}).toArray((e, result) => {
-        console.log(`${time.getTime()}` + chalk.green `GET` + ":200");
-        res.status(200).json(result);
-      })
-    } catch (error) {
-      console.log(`${time.getTime()}` + chalk.red `GET` + ":500");
-      console.error(error);
-      res.status(500).json({
-        status: 'error'
-      });
-    }
-  });
-  router.put('/teamWorker/:id', function (req, res, next) {
-    try {
-      col.updateOne({
-        _id: new mongodb.ObjectId(req.params.id)
-      }, {
-        $set: {
-          name: req.body.name,
-        surname: req.body.surname,
-        project: req.body.project,
-        imgLink: req.body.imgLink
-        }
-      }).then(() => {
-        console.log(`${time.getTime()}` + chalk.green `PUT` + ":200");
-        res.status(200).json({
-          status: 'ok'
-        });
-      })
-    } catch (error) {
-      console.log(`${time.getTime()}` + chalk.red `PUT` + ":500");
-      console.error(error);
-      res.status(500).json({
-        status: 'error'
-      });
-    }
-  });
-  router.delete('/teamWorker/:id', function (req, res, next) {
-    try {
-      col.deleteOne({
-        _id: new mongodb.ObjectId(req.params.id),
-      }).then(() => {
-        console.log(`${time.getTime()}` + chalk.green `DELETE` + ":200");
-        res.status(200).json({
-          status: 'ok'
-        });
-      })
-    } catch (error) {
-      console.log(`${time.getTime()}` + chalk.red `DELETE` + ":500");
-      console.error(error);
-      res.status(500).json({
-        status: 'error'
-      });
-    }
-  });
-  return router;
+function findUserIdForEmail(user) {
+    let ans = col.findOne(user, {
+        _id: 1
+    })
+    return ans 
 }
-module.exports = teamWorkerRouter
+
+function validateEmailAndPassword(user) {
+    let ans = col.findOne(user) || false
+    if (ans != false) ans = true
+    return ans
+}
+
+function loginRouter(client) {
+    db = client.db("CAD")
+    col = db.collection("login")
+
+    const router = new express.Router()
+    router.post('/login', (req, res, next) => {
+        const user = {
+            email: req.body.email,
+            password: req.body.password
+        }
+        try {
+            if (validateEmailAndPassword()) {
+                const userId = findUserIdForEmail(user.email);
+
+                const jwtBearerToken = jwt.sign({}, RSA_PRIVATE_KEY, {
+                    algorithm: 'RS256',
+                    expiresIn: 120,
+                    subject: userId
+                })
+                console.log(`${time.getTime()}`+chalk.green`POST`+ chalk.yellow` login :200 ${user.email}`);
+                res.status(200).json({
+                    status: 'ok',
+                    token: jwtBearerToken
+                });
+            } else {
+                throw "401"
+            }
+        } catch (error) {
+            console.log(`${time.getTime()}` +chalk.red`POST`+ chalk.yellow` login ${error} ${user.email}` );
+            console.error(error);
+            res.status(401).json({
+                status: 'Unauthorized'
+            });
+        }
+    });
+
+    return router
+}
+module.exports = loginRouter
--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express')
+const login = require('./scripts/login');
+
+
 const {
     MongoClient
 } = require('mongodb')
 const cors = require('cors')
+// const cookieParser = require('cookie-parser');
+
 const projectRouter = require('./scripts/projectRouter')
 const teamWorkerRouter = require('./scripts/teamWorkerRouter')
 
@@ -27,8 +32,10 @@ async function main() {
         .use(express.urlencoded({
             extended: true
         }))
+        // .use(cookieParser.JSONCookie)
         .use(projectRouter(client))
         .use(teamWorkerRouter(client))
+        .use(login(client))
         .use(express.static('assets'))
     if(app.get('env')!="development")app.use(requireHTTPS);
     app.listen(port, () => {
